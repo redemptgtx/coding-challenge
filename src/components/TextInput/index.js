819b04ab21019b99043ffe2f8b4be9a626c65a44
Serkan <serkan@Bunyamin-MacBook-Pro.local>
2020-03-23 19:13:37 +0300

    Unused style file removed. Mistake fixed on textinput props omit.

--- src/components/TextInput/index.js
+++ src/components/TextInput/index.js
@@ -4,12 +4,12 @@ import { omit } from "lodash";
 import * as S from "./style";
 
 const TextInput = props => {
-  const { label, placeholder } = props;
-  const inputProps = omit(props, ["label", "placeholder"]);
+  const { label } = props;
+  const inputProps = omit(props, ["label"]);
   return (
     <S.InputContainer>
       <S.InputLabel>{label}</S.InputLabel>
-      <S.TextInput placeholder={placeholder} {...inputProps} />
+      <S.TextInput {...inputProps} />
     </S.InputContainer>
   );
 };
